fd65974a60308a281bb4c03bbde9dbc039f5d8cc
Lukasz Mordasiewicz <l.mordasiewicz@gmail.com>
2021-09-17 20:42:03 +0200

    add next pesel tests

--- src/pesel.spec.ts
+++ src/pesel.spec.ts
@@ -1,7 +1,7 @@
 import { getPesel } from './pesel';
 import { Pesel } from './types';
 
-describe('test peles.ts', () => {
+describe('test pesel.ts', () => {
   let pesel: Pesel;
 
   beforeEach(() => {
@@ -10,6 +10,17 @@ describe('test peles.ts', () => {
 
   it('should be string and have 11 digits', () => {
     expect(typeof pesel).toBe('string');
-    expect(pesel.toString().length).toBe(11);
+    expect(pesel.length).toBe(11);
+  });
+
+  it('should return only digits', () => {
+    expect(/^\d+$/.test(pesel)).toBeTruthy();
+  });
+
+  it('should return random pesels', () => {
+    const pesels = [...new Array(10)].map(getPesel);
+    const peselsWithoutDulications = [...new Set(pesels)];
+
+    expect(peselsWithoutDulications.length).toBeGreaterThan(1);
   });
 });
