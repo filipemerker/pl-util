5babac28d6c12bfff06fde70762cfa7307ab114c
Lukasz Mordasiewicz <l.mordasiewicz@gmail.com>
2021-09-21 10:42:06 +0200

    test names

--- src/personName.spec.ts
+++ src/personName.spec.ts
@@ -1,9 +1,12 @@
-import { getPersonName } from './personName';
+import { getPersonName, getPersonSurname, getPersonFullName } from './personName';
 
 import FEMALE_NAMES from './data/femaleNames.json';
+import FEMALE_SURNAMES from './data/femaleSurnames.json';
 import MALE_NAMES from './data/maleNames.json';
+import MALE_SURNAMES from './data/maleSurnames.json';
 
-describe('test personName.ts', () => {
+// test names
+describe('test person name', () => {
   let personName: string;
 
   beforeEach(() => {
@@ -24,8 +27,68 @@ describe('test personName.ts', () => {
   });
 
   it('should return proper man name', () => {
-    // test with attribute
     personName = getPersonName('woman');
     expect(FEMALE_NAMES.indexOf(personName)).toBeGreaterThan(-1);
+  });
+});
+
+// test surnames
+describe('test person surname', () => {
+  let personSurname: string;
+
+  beforeEach(() => {
+    personSurname = getPersonSurname();
+  });
+
+  it('should return string and value', () => {
+    expect(typeof personSurname).toBe('string');
+    expect(personSurname.length).toBeGreaterThan(1);
+  });
+
+  it('should return proper man surname', () => {
+    expect(MALE_SURNAMES.indexOf(personSurname)).toBeGreaterThan(-1);
+
+    // test with attribute
+    personSurname = getPersonSurname('man');
+    expect(MALE_SURNAMES.indexOf(personSurname)).toBeGreaterThan(-1);
+  });
+
+  it('should return proper man surname', () => {
+    personSurname = getPersonSurname('woman');
+    expect(FEMALE_SURNAMES.indexOf(personSurname)).toBeGreaterThan(-1);
+  });
+});
+
+// test fullnames
+describe('test person fullname', () => {
+  let personFullname: string;
+
+  beforeEach(() => {
+    personFullname = getPersonFullName();
+  });
+
+  it('should return string and value', () => {
+    expect(typeof personFullname).toBe('string');
+    expect(personFullname.length).toBeGreaterThan(1);
+  });
+
+  it('should return proper man fullname', () => {
+    const [name, surname] = personFullname.split(' ');
+    console.log(name, MALE_NAMES.indexOf(name));
+    expect(MALE_NAMES.indexOf(name)).toBeGreaterThan(-1);
+    expect(MALE_SURNAMES.indexOf(surname)).toBeGreaterThan(-1);
+
+    // test with attribute
+    personFullname = getPersonFullName('man');
+    const [name2, surname2] = personFullname.split(' ');
+    expect(MALE_NAMES.indexOf(name2)).toBeGreaterThan(-1);
+    expect(MALE_SURNAMES.indexOf(surname2)).toBeGreaterThan(-1);
+  });
+
+  it('should return proper man fullname', () => {
+    personFullname = getPersonFullName('woman');
+    const [name, surname] = personFullname.split(' ');
+    expect(FEMALE_NAMES.indexOf(name)).toBeGreaterThan(-1);
+    expect(FEMALE_SURNAMES.indexOf(surname)).toBeGreaterThan(-1);
   });
 });
--- src/personName.ts
+++ src/personName.ts
@@ -2,12 +2,38 @@ import { getRandomBetween } from './utils';
 import { Gender } from './types';
 
 import FEMALE_NAMES from './data/femaleNames.json';
+import FEMALE_SURNAMES from './data/femaleSurnames.json';
 import MALE_NAMES from './data/maleNames.json';
+import MALE_SURNAMES from './data/maleSurnames.json';
+
+const getName = <T>(items: T[]) => {
+  const genderNamesLength = items.length;
+  const genderNameRandomIndex = getRandomBetween(0, genderNamesLength);
+
+  return items[genderNameRandomIndex];
+};
 
 export const getPersonName = (gender: Gender = 'man') => {
   const isMan = gender === 'man';
-  const genderNamesLength = isMan ? MALE_NAMES.length : FEMALE_NAMES.length;
-  const genderNameRandomIndex = getRandomBetween(0, genderNamesLength);
+  const items = isMan ? MALE_NAMES : FEMALE_NAMES;
 
-  return isMan ? MALE_NAMES[genderNameRandomIndex] : FEMALE_NAMES[genderNameRandomIndex];
+  return getName(items);
+};
+
+export const getPersonSurname = (gender: Gender = 'man') => {
+  const isMan = gender === 'man';
+  const items = isMan ? MALE_SURNAMES : FEMALE_SURNAMES;
+
+  return getName(items);
+};
+
+export const getPersonFullName = (gender: Gender = 'man') => {
+  const isMan = gender === 'man';
+  const names = isMan ? MALE_NAMES : FEMALE_NAMES;
+  const name = getName(names);
+
+  const surnames = isMan ? MALE_SURNAMES : FEMALE_SURNAMES;
+  const surname = getName(surnames);
+
+  return `${name} ${surname}`;
 };
